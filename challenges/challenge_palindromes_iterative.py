95644cec263c5df3aab99439cfd769701f2d9671
murielpaoli <murielpaoli@gmail.com>
2024-02-29 21:43:12 -0300

    req 6

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,22 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    # Se a palavra estiver vazia, retorna False
+    if not word:
+        return False
+
+    # Inicializa os índices low_index e high_index
+    low_index = 0
+    high_index = len(word) - 1
+
+    # Enquanto low_index for menor que high_index, verifica se os caracteres
+    # nos índices low_index e high_index são iguais
+    while low_index < high_index:
+        # Se os caracteres não forem iguais, a palavra não é um palíndromo
+        if word[low_index] != word[high_index]:
+            return False
+
+        # Atualiza os índices low_index e high_index
+        low_index += 1
+        high_index -= 1
+
+    # Se o loop terminar, a palavra é um palíndromo
+    return True
